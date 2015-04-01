65955205dbdafa8f87b66c62c299822c0c939599
mmerhej <merhej.com@gmail.com>
2015-04-01 17:47:47 +0300

    report
    enabled jasper reports also added a report sample in pdf, working on
    documentation and enabling export to pdf module

--- build/generated-sources/ap-source-output/imported_from_db/Operations_.java
+++ build/generated-sources/ap-source-output/imported_from_db/Operations_.java
@@ -7,7 +7,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-02-23T21:26:09")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-04-01T17:34:18")
 @StaticMetamodel(Operations.class)
 public class Operations_ { 
 
